3331abc3b1dba88c8477fdb560d13cd646404f46
Dale <Danntabachoyy@gmail.com>
2023-09-09 18:27:31 +0800

    created a handleAdd for newTodo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState, useEffect } from "react";
 import axios from "./utils/axiosInstance";
 import Sidebar from "./sidebar";
 import MainContent from "./mainContent";
+import date from "./utils/date";
 function App() {
   const [todos, setTodos] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
   const retrieveData = async () => {
     const res = await axios.get("/Todos");
     return res.data;
+  };
+  const handleAdd = async (title) => {
+    const nextId = todos.length;
+    const newTodo = { id: nextId, title: title, date: date() };
+    try {
+      const res = await axios.post("/Todos", newTodo);
+      setTodos((oldTodos) => [...oldTodos, res.data]);
+    } catch {
+      console.log("error");
+    }
   };
   // intial fetch
   useEffect(() => {
@@ -25,9 +38,16 @@ function App() {
   return (
     <div className="flex flex-col lg:grid lg:flex-none lg:grid-cols-4 container mx-auto h-screen">
       <Sidebar />
-      <MainContent todos={todos} />
+      <MainContent
+        todos={todos}
+        showModal={showModal}
+        handleAdd={handleAdd}
+        setShowModal={setShowModal}
+      />
     </div>
   );
 }
 
+// added a modal
+// add a function that handlesTheAdd
 export default App;
--- src/mainContentComp/todos.jsx
+++ src/mainContentComp/todos.jsx
@@ -1,13 +1,23 @@
+/* eslint-disable react/prop-types */
 import TodosHeader from "./todosHeader";
 import TodosBody from "./todosBody";
+import Modal from "../modal";
 function Todos(props) {
   return (
     <div className="flex flex-col p-4 space-y-5">
       <TodosHeader />
-      <TodosBody {...props}/>
+      <TodosBody {...props} />
       <div className="text-right">
-        <button className="py-4 px-8 bg-blue-400 text-white hover:bg-blue-500 rounded font-semibold">Add new task</button>
+        <button
+          className="py-4 px-8 bg-blue-400 text-white hover:bg-blue-500 rounded font-semibold"
+          onClick={() => {
+            props.setShowModal(true);
+          }}
+        >
+          Add new task
+        </button>
       </div>
+      {props.showModal && <Modal handleAdd={props.handleAdd} setShowModal={props.setShowModal} />}
     </div>
   );
 }
